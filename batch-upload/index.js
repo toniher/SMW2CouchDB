7b31d53f0e8d20a6646abbd0616bf0c46807f64e
toniher <toniher@cau.cat>
2015-03-04 14:36:41 +0100

    process.js

--- batch-upload/index.js
+++ batch-upload/index.js
@@ -1,10 +1,11 @@
 var nconfig = require('./config.js');
 var process = require('./process.js');
 
-var config = nconfig.get("");
+var config = nconfig.get("config");
 
 process.getSMWBlastDBcmd( config, function( cb ) {
 
+	console.log( typeof cb );
 	console.log( cb );
 	return true;
 	
