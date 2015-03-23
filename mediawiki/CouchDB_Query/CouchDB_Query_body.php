dc1cf6a8d6e8258c9765140168bc4fc2c9e96f46
toniher <toniher@cau.cat>
2015-03-23 11:32:29 +0100

    more params

--- mediawiki/CouchDB_Query/CouchDB_Query_body.php
+++ mediawiki/CouchDB_Query/CouchDB_Query_body.php
@@ -12,44 +12,52 @@ class CouchDB_Query {
 	public static function process_CouchDB_Query_table( $parser, $frame, $args ) {
 
 		// Get data from parser args and to data
-                // Defaults
-                
-                $limit =  "25";
-                $header = "Page name";
-                $smw = "*";
-                $type = "";
-                $index = "";
-                $query = "";
-                $class = "wikitable sortable";
-                
-                // TODO: Pending parsing args?
-                
-                if ( key_exists( "limit", $args ) ) {
-                    $limit = $args["limit"];
-                }
-                if ( key_exists( "header", $args ) ) {
-                    $header = $args["header"];
-                }
-                if ( key_exists( "smw", $args ) ) {
-                    $smw = $args["smw"];
-                }
-                if ( key_exists( "type", $args ) ) {
-                    $type = $args["type"];
-                }
-                if ( key_exists( "index", $args ) ) {
-                    $index = $args["index"];
-                }
-                if ( key_exists( "query", $args ) ) {
-                    $query = $args["query"];
-                }
-                if ( key_exists( "class", $args ) ) {
-                    $class = $args["class"];
-                }
-                
+		// Defaults
+		
+		$limit =  "25";
+		$header = "Page name";
+		$smw = "*";
+		$type = "";
+		$index = "";
+		$query = "";
+		$class = "wikitable sortable";
+		$startstr = "";
+		$endstr = "";
+		
+		// TODO: Pending parsing args?
+		
+		if ( key_exists( "limit", $args ) ) {
+			$limit = $args["limit"];
+		}
+		if ( key_exists( "header", $args ) ) {
+			$header = $args["header"];
+		}
+		if ( key_exists( "smw", $args ) ) {
+			$smw = $args["smw"];
+		}
+		if ( key_exists( "type", $args ) ) {
+			$type = $args["type"];
+		}
+		if ( key_exists( "index", $args ) ) {
+			$index = $args["index"];
+		}
+		if ( key_exists( "query", $args ) ) {
+			$query = $args["query"];
+		}
+		if ( key_exists( "class", $args ) ) {
+			$class = $args["class"];
+		}
+		if ( key_exists( "start", $args ) ) {
+			$startstr = " data-start = '".$args["start"]."'";
+		}
+		if ( key_exists( "end", $args ) ) {
+			$endstr = " data-end = '".$args["end"]."'";
+		}
+
 		$out = $parser->getOutput();
 		$out->addModules( 'ext.CouchDB_Query' );
 
-		$returnhtml = "<div class='couchdb-query-table' data-total=0 data-class='$class' data-limit='$limit' data-header='$header' data-smw='$smw' data-query='$query' data-index='$index' data-type='$type' data-skip=0 ></div>";
+		$returnhtml = "<div class='couchdb-query-table' data-total=0 data-class='$class' $startstr $endstr data-limit='$limit' data-header='$header' data-smw='$smw' data-query='$query' data-index='$index' data-type='$type' data-skip=0 ></div>";
 
 		return array( $returnhtml, 'noparse' => true, 'isHTML' => true );
 
