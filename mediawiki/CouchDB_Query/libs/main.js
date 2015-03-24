ba224d83d43bb58d56f60927f8c7be51207524a1
toniher <toniher@cau.cat>
2015-03-24 15:38:22 +0100

    input

--- mediawiki/CouchDB_Query/libs/main.js
+++ mediawiki/CouchDB_Query/libs/main.js
@@ -9,7 +9,7 @@
 	
 
 	// Look for changes in the value
-	$( ".couchdb-query-table" ).delegate("input", "propertychange change click keyup input paste", function(event){
+	$( ".couchdb-query-table" ).on( "propertychange change click keyup input paste", "input", function(event){
 		// If value has changed...
 		if ( inputsave !== $(this).val()) {
 			// Updated stored value
