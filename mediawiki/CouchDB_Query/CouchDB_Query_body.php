c2cb1c5c4d1b9e8f14c43523997d49e623797a5a
toniher <toniher@cau.cat>
2015-03-30 15:53:36 +0200

    adding query field

--- mediawiki/CouchDB_Query/CouchDB_Query_body.php
+++ mediawiki/CouchDB_Query/CouchDB_Query_body.php
@@ -21,6 +21,7 @@ class CouchDB_Query {
 		$attrs["type"] = "";
 		$attrs["index"] = "";
 		$attrs["query"] = "";
+		$attrs["extra"] = "";
 		$attrs["class"] = "wikitable sortable jquery-tablesorter";
 		$attrs["db"] = $GLOBALS["wgDBname"]; //Default DB
 		$startstr = "";
@@ -50,7 +51,7 @@ class CouchDB_Query {
 		$out = $parser->getOutput();
 		$out->addModules( 'ext.CouchDB_Query' );
 
-		$returnhtml = "<div class='couchdb-query-table' data-text='".$attrs["text"]."' data-total=0 data-skip=0 data-class='".$attrs["class"]."' data-db='".$attrs["db"]."' ".$startstr.$endstr;
+		$returnhtml = "<div class='couchdb-query-table' data-text='".$attrs["text"]."' data-extra='".$attrs["extra"]."' data-total=0 data-skip=0 data-class='".$attrs["class"]."' data-db='".$attrs["db"]."' ".$startstr.$endstr;
 		$returnhtml.= " data-limit='".$attrs["limit"]."' data-header='".$attrs["header"]."' data-fields='".$attrs["fields"]."' data-query='".$attrs["query"]."' data-index='".$attrs["index"]."' data-type='".$attrs["type"]."'></div>";
 
 		return array( $returnhtml, 'noparse' => true, 'isHTML' => true );
@@ -67,6 +68,32 @@ class CouchDB_Query {
 	public static function process_CouchDB_Query_field( $parser, $frame, $args ) {
 
 		$returnhtml = "";
+
+		$attrs["tag"] = "input";
+		$attrs["type"] = "text";
+		$attrs["query"] = "";
+		$attrs["values"] = "";
+		$attrs["class"] = "";
+		$attrs["id"] = "";
+
+
+		$attrs_ref = array( "tag", "type", "query", "values", "class", "id" );
+
+		foreach ( $args as $arg ) {
+			$arg_clean = trim( $frame->expand( $arg ) );
+			$arg_proc = explode( "=", $arg_clean, 2 );
+			
+			if ( count( $arg_proc ) == 2 ){
+			
+				if ( in_array( trim( $arg_proc[0] ), $attrs_ref ) ) {
+					$attrs[ trim( $arg_proc[0] ) ] = trim( $arg_proc[1] );
+				}
+			}
+		}
+
+
+		$returnhtml = "<div class='couchdb-query-field' data-tag='".$attrs["tag"]."' data-type='".$attrs["type"]."' data-values='".$attrs["values"]."' data-query='".$attrs["query"]."' data-class='".$attrs["class"]."' data-id='".$attrs["id"]."'></div>";
+
 
 		// Return HTML
 		return array( $returnhtml, 'noparse' => true, 'isHTML' => true );
