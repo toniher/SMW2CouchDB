7b2be489a5965a17e6a9c9943174410658e9a4f0
toniher <toniher@cau.cat>
2015-03-30 15:59:00 +0200

    field

--- mediawiki/CouchDB_Query/CouchDB_Query.i18n.magic.php
+++ mediawiki/CouchDB_Query/CouchDB_Query.i18n.magic.php
@@ -4,5 +4,6 @@ $magicWords = array();
 
 /** English (English) */
 $magicWords['en'] = array(
-	'CouchDB_Query_table' => array( 0, 'CouchDB_Query_table' )
+	'CouchDB_Query_table' => array( 0, 'CouchDB_Query_table' ),
+	'CouchDB_Query_field' => array( 0, 'CouchDB_Query_field' )
 );
